7658ac1a897d2bbb622fd4f9883f84edc5c30558
Icetear34 <kimoqq00@gmail.com>
2024-07-30 15:45:07 +0800

    refactor: Rename App.vue and add WelcomeCompoments.vue
    The commit renames the file App.vue to Chapter03/vue-project/src/App.vue and adds a new file WelcomeCompoments.vue to Chapter03/vue-project/src/components. This refactor improves the structure of the project by organizing the components into separate files and directories.

---
+++ Chapter03/vue-project/src/components/WelcomeCompoments.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+       <label>{{ labelText }}</label>
+       <input type="text" v-model="inputText">
+       <button v-on:click="$emit('sathello',inputText)">sathello</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'WelcomeCompoments',
+    props: {
+        labelText: {
+            type: String,
+            default: 'Hello'
+        },
+        inputText: {
+            type: String,
+            default: ''
+        }
+    },
+    data() {
+        return {
+            labelText: 'Hello',
+            inputText: ''
+        }
+    }
+}
+</script>
