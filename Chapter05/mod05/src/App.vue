147b8ee3a56e65788af770f60396e717b6996f7f
Icetear34 <kimoqq00@gmail.com>
2024-07-31 12:19:15 +0800

    feat: Add Vite configuration and update dependencies

---
+++ Chapter05/mod05/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h1 class ="display-3">Posts App </h1>
+    <h1 class="h6">Seeing all posts</h1>
+    <Posts />
+  </div>
+</template>
+
+<script setup>
+//@ is an alias for /src
+import Posts from '@/components/Posts.vue';
+</script>
+
+<style >
+#app {  
+  color: #2c3e50;
+  padding: 50px;  
+  background-color: rgb(252,233,198);
+}
+#box {
+  background-color: #f0f0f0 !important; 
+}
+</style>
---
+++ Chapter05/mod05/src/components/Posts.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h3> Post </h3>
+        <ul class="list-group">
+            <li class="list-group-item" v-for="p in getAllPosts" v-bind:key="p.Id">                
+                {{ p.title }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { usePostStore } from "@/store/PostStore.js";
+const store = usePostStore();
+const getAllPosts = computed(() => 
+    store.getAllPosts
+)
+
+</script>
+
+
+<style  scoped>
+.list-group-item{
+    color: brown;
+}
+</style>
